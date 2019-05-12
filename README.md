eefaf1a878f3818bd4ec23bae78313c48e26345e
Andrew Rogers <tuxlovesyou@gmail.com>
2019-05-12 07:07:35 -0400

    added first helper and a nifty readme

---
+++ README.md
@@ -0,0 +1,25 @@
+# GitHub Helpers
+Some handy little friends that make using GitHub a little easier.
+
+## Known Hosts Helper
+`ssh_helper.py` is a simple Python script that prints valid `known_hosts`
+entries to STDOUT based on the IP ranges listed in the `git` block at the
+GitHub API's `/meta` endpoint.
+
+It is a pretty stupid program as of now, but it does work.  Eventually I will
+try to consolidate `/24` subnets into globs, where possible.  For now, feast on
+your new >6000 line `known_hosts` file!
+
+### DEPENDENCIES
+I have not added a `requirements` file of a `setup.py` file yet, but there is
+only a single dependency as of now: [ipcalc](https://github.com/tehmaze/ipcalc).
+
+You can grab it with this one-liner (you may need to use `sudo` here):
+```
+you@yourbox $ pip3 install ipcalc
+```
+
+### USAGE
+```
+you@yourbox $ ./ssh_helper.py
+```
---
+++ ssh_helper.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import requests
+import ipcalc
+
+# This is GitHub's public RSA host key.
+gh_pubkey  = 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa' \
+           + '+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJ' \
+           + 'NlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5Q' \
+           + 'lWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaas' \
+           + 'XVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua' \
+           + '2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=='
+
+# GitHub provides a list of their Git servers via their API's /meta endpoint.
+req = requests.get('https://api.github.com/meta')
+gitnets = req.json()['git']
+
+for net in gitnets:
+  for ip in ipcalc.Network(net):
+    gitservers = ['github.com']
+    gitservers.append(ip.to_compressed())
+    s_gitservers = ','.join(gitservers)
+    print(s_gitservers + ' ' + gh_pubkey)
